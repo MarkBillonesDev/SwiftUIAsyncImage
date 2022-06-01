333d28b02db8e4adb8c8727fbe7165015c96c478
Mark Billones <mark.billones@opsolutions.biz>
2022-06-01 18:29:16 +0800

    first commit

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by OPSolutions on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
